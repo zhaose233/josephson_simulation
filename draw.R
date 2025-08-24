63796d763aeb2946c2b89ec796ee113373202465
zhaose233 <zhaose233@outlook.com>
2025-08-24 15:10:46 +0800

    R update

--- draw.R
+++ draw.R
@@ -2,6 +2,6 @@ d <- read.csv("out.csv")
 dd <- read.csv("out_driving.csv")
 # d$delta_phi <- d$phi - c(0,d$phi[1:length(d$phi) - 1])
 
-plot(d$Ib, d$V, type = "l")
+plot(d$V, d$Ib, type = "l")
 
 plot(dd$t, dd$V, type = "l")
